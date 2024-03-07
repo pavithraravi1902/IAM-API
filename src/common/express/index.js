6ff4dc1daa82001e9535ee88b95941dd8c176995
Pavithra R <pavithrar@bloomlync.com>
2024-03-07 14:53:48 +0530

    unused code removed

--- src/common/express/index.js
+++ src/common/express/index.js
@@ -3,10 +3,8 @@ import { errorHandler as bodyErrorHandler } from "bodymen";
 import compression from "compression";
 import cors from "cors";
 import express from "express";
-import forceSSL from "express-force-ssl";
 import morgan from "morgan";
 import { errorHandler as queryErrorHandler } from "querymen";
-import { env } from "../../config";
 var path = require("path");
 
 export default (apiRoot, routes) => {
@@ -19,15 +17,6 @@ export default (apiRoot, routes) => {
   app.get("/", (req, res) => {
     res.render("index.ejs");
   });
-
-  /* istanbul ignore next */
-  if (env === "production") {
-    app.set("forceSSLOptions", {
-      enable301Redirects: false,
-      trustXFPHeader: true,
-    });
-    app.use(forceSSL);
-  }
 
   app.use(cors());
   app.options("*", cors());
